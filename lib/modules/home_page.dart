8401e8b696196dc5b7a37452a0affaddd1d36224
raymond <yaoyu.liao@highsoft.ltd>
2023-03-10 09:52:51 +0800

    feat: Add TextField

--- lib/modules/home_page.dart
+++ lib/modules/home_page.dart
@@ -20,29 +20,51 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    onSubmitted: (value) {
+                      debugPrint("TextField onSubmitted value: $value");
+                    },
+                    onChanged: (value) {
+                      debugPrint("TextField onChanged value: $value");
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Input here",
+                        filled: true,
+                        contentPadding: const EdgeInsets.all(20),
+                        border: OutlineInputBorder(gapPadding: 0, borderRadius: BorderRadius.circular(4), borderSide: const BorderSide(color: Color(0xFFEAEAEA)))),
+                  ),
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ));
   }
 }
